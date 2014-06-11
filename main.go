fdc52a955d70536172802c8006cb8686138a2232
Ben Burkert <ben@benburkert.com>
2014-06-11 08:01:12 -0700

    Add some simple flag parsing.

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
+
+var addr = flag.String("address", "0.0.0.0", "Bind to host address (default is 0.0.0.0)")
+var port = flag.String("port", "3000", "Bind to host port (default is 3000)")
+var help = flag.Bool("help", false, "Print help and exit")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "TransferEncoding: %s", r.TransferEncoding[0])
 }
 
 func main() {
+	flag.Parse()
+
+	if *help {
+		fmt.Println("htsd - ht.io server")
+		flag.PrintDefaults()
+
+		os.Exit(0)
+	}
+
+	addrPort := *addr + ":" + *port
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(addrPort, nil)
 }
