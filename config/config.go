6e002735de2c089dcc65e953c13a5979e73ba8b0
Ben Burkert <ben@benburkert.com>
2014-06-11 15:00:32 -0700

    Move the config stuff to a seperate file.

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Addr     string
+	Port     int
+	RedisUrl string
+}
+
+func New() *Config {
+	c := new(Config)
+
+	addr := flag.String("address", "0.0.0.0", "Bind to host address (default is 0.0.0.0)")
+	port := flag.String("port", "3000", "Bind to host port (default is 3000)")
+
+	flag.StringVar(&c.RedisUrl, "redis-url", "", "Redis server URL (default is REDIS_URL)")
+
+	help := flag.Bool("help", false, "Print help and exit")
+
+	flag.Parse()
+
+	if *help {
+		fmt.Println("htsd - ht.io server")
+		flag.PrintDefaults()
+
+		os.Exit(0)
+	}
+
+	c.Addr = *addr + ":" + *port
+
+	return c
+}
--- htsd.go
+++ htsd.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
-	"net/url"
-	"os"
-)
 
-var addr = flag.String("address", "0.0.0.0", "Bind to host address (default is 0.0.0.0)")
-var port = flag.String("port", "3000", "Bind to host port (default is 3000)")
-var redis = flag.String("redis-url", os.Getenv("REDIS_URL"), "Redis server URL (default is REDIS_URL)")
-var help = flag.Bool("help", false, "Print help and exit")
+	"github.com/htio/htsd/config"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "TransferEncoding: %s", r.TransferEncoding[0])
 }
 
 func main() {
-	flag.Parse()
-
-	if *help {
-		fmt.Println("htsd - ht.io server")
-		flag.PrintDefaults()
-
-		os.Exit(0)
-	}
-
-	addrPort := *addr + ":" + *port
-	url.Parse(*redis)
+	c := config.New()
 
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(addrPort, nil)
+	http.ListenAndServe(c.Addr, nil)
 }
