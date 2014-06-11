8686b3a36f0531e1d1ca55e419359583a72e4685
Ben Burkert <ben@benburkert.com>
2014-06-11 11:22:13 -0700

    Rename main.go to htsd.go.

--- htsd.go
+++ htsd.go
@@ -4,11 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 var addr = flag.String("address", "0.0.0.0", "Bind to host address (default is 0.0.0.0)")
 var port = flag.String("port", "3000", "Bind to host port (default is 3000)")
+var redis = flag.String("redis-url", os.Getenv("REDIS_URL"), "Redis server URL (default is REDIS_URL)")
 var help = flag.Bool("help", false, "Print help and exit")
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +28,7 @@ func main() {
 	}
 
 	addrPort := *addr + ":" + *port
+	url.Parse(*redis)
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(addrPort, nil)
