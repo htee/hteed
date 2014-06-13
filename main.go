5e265cc1985936be644c7a03651778121d379f50
Ben Burkert <ben@benburkert.com>
2014-06-13 13:25:14 -0700

    Hack out the GET endpoint.

--- main.go
+++ main.go
@@ -20,16 +20,12 @@ var (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.TransferEncoding[0] != "chunked" {
-		w.WriteHeader(404)
-	}
-
 	switch r.Method {
 	case "POST":
 		postHandler(w, r)
+	case "GET":
+		getHandler(w, r)
 	}
-
-	fmt.Fprintf(w, "TransferEncoding: %s", r.TransferEncoding[0])
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,25 +37,23 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	var n int
 	var err error
 
-	stateKey := "state:" + r.URL.Path[1:]
-	dataKey := "data:" + r.URL.Path[1:]
-	streamKey := r.URL.Path[1:] // pub-sub channels don't show up in KEYS *
+	conn.Do("SET", stateKey(r), Streaming)
 
-	conn.Do("SET", stateKey, Streaming)
-
-	defer conn.Do("SET", stateKey, Fin)
+	defer conn.Do("SET", stateKey(r), Fin)
 
 	for {
 		n, err = r.Body.Read(buf)
 
-		if err != nil && err != io.EOF {
+		if err == io.EOF {
+			conn.Send("PUBLISH", streamKey(r), []byte{Fin})
+		} else if err != nil {
 			fmt.Println(err.Error())
 		}
 
 		if n > 0 {
 			conn.Send("MULTI")
-			conn.Send("APPEND", dataKey, buf[:n])
-			conn.Send("PUBLISH", streamKey, append([]byte{Streaming}, buf[:n]...))
+			conn.Send("APPEND", dataKey(r), buf[:n])
+			conn.Send("PUBLISH", streamKey(r), append([]byte{Streaming}, buf[:n]...))
 			conn.Send("EXEC")
 			conn.Flush()
 			fmt.Printf("%s", buf[:n])
@@ -72,7 +66,76 @@ respond:
 	conn.Do("PUBLISH", streamKey, []byte{Fin})
 
 	w.WriteHeader(204)
-	fmt.Fprintf(w, "TransferEncoding: %s", r.TransferEncoding[0])
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	conn := pool.Get()
+	defer conn.Close()
+
+	state, err := redis.Int(conn.Do("GET", stateKey(r)))
+	if err != nil {
+		w.WriteHeader(500)
+		panic(err)
+	}
+
+	switch state {
+	case Streaming:
+		psc := redis.PubSubConn{conn}
+		data, err := redis.String(conn.Do("GET", dataKey(r)))
+
+		if err != nil {
+			w.WriteHeader(500)
+			panic(err)
+		}
+
+		psc.Subscribe(streamKey(r))
+
+		w.WriteHeader(200)
+		w.Write([]byte(data))
+		w.(http.Flusher).Flush()
+
+		for {
+			switch v := psc.Receive().(type) {
+			case redis.Message:
+				switch v.Data[0] {
+				case Streaming:
+					w.Write(v.Data[1:])
+					w.(http.Flusher).Flush()
+				case Fin:
+					w.(http.Flusher).Flush()
+					return
+				}
+			case error:
+				panic(v)
+			}
+		}
+
+	case Fin:
+
+		data, err := redis.String(conn.Do("GET", dataKey(r)))
+
+		if err != nil {
+			w.WriteHeader(500)
+			panic(err)
+		}
+
+		w.WriteHeader(200)
+		w.Write([]byte(data))
+		w.(http.Flusher).Flush()
+
+	}
+}
+
+func stateKey(r *http.Request) string {
+	return "state:" + streamKey(r)
+}
+
+func dataKey(r *http.Request) string {
+	return "data:" + streamKey(r)
+}
+
+func streamKey(r *http.Request) string {
+	return r.URL.Path[1:] // pub-sub channels don't show up in KEYS *
 }
 
 func redisPool(url string) *redis.Pool {
