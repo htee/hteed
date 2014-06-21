d8b39f98c09dae8b90383e3c9a81394503d376b3
Ben Burkert <ben@benburkert.com>
2014-06-20 22:01:15 -0700

    Break out the stream playback stuff too.

--- httd.go
+++ httd.go
@@ -29,10 +29,10 @@ func recordStream(_ http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
 	conn := pool.Get()
 	defer conn.Close()
 
-	if stream, err := stream.In(owner, name, conn); err != nil {
+	if in, err := stream.In(owner, name, conn); err != nil {
 		w.WriteHeader(500)
 	} else {
-		if _, err = stream.Fill(r.Body); err != nil {
+		if _, err = in.From(r.Body); err != nil {
 			w.WriteHeader(500)
 		} else {
 			w.WriteHeader(204)
@@ -41,72 +41,24 @@ func recordStream(_ http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
 }
 
 func playbackStream(_ http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path[1:], "/")
+	owner, name := parts[0], parts[1]
+
 	conn := pool.Get()
 	defer conn.Close()
 
-	state, err := redis.Int(conn.Do("GET", stateKey(r)))
-	if err != nil {
+	if out, err := stream.Out(owner, name, conn); err != nil {
 		w.WriteHeader(500)
-		panic(err)
-	}
-
-	switch state {
-	case Streaming:
-		psc := redis.PubSubConn{conn}
-
-		conn.Send("MULTI")
-		conn.Send("GET", dataKey(r))
-		conn.Send("SUBSCRIBE", streamKey(r))
-		if results, err := redis.Values(conn.Do("EXEC")); err != nil {
-			w.WriteHeader(500)
-			panic(err)
-		} else {
-			var buf []byte
-			redis.Scan(results, &buf)
-
-			w.WriteHeader(200)
-			w.Write(buf)
-			w.(http.Flusher).Flush()
-		}
-
-		for {
-			switch v := psc.Receive().(type) {
-			case redis.Message:
-				switch v.Data[0] {
-				case Streaming:
-					w.Write(v.Data[1:])
-					w.(http.Flusher).Flush()
-				case Fin:
-					hj, _ := w.(http.Hijacker)
-					conn, bufrw, _ := hj.Hijack()
-
-					bufrw.WriteString("0\r\n\r\n")
-					bufrw.Flush()
-					conn.Close()
-
-					return
-				}
-			case error:
-				panic(v)
-			}
-		}
-
-	case Fin:
-
-		data, err := redis.String(conn.Do("GET", dataKey(r)))
-
-		if err != nil {
-			w.WriteHeader(500)
-			panic(err)
-		}
-
+	} else {
 		w.WriteHeader(200)
-		w.Write([]byte(data))
-		w.(http.Flusher).Flush()
+		out.To(w)
 
-		if sse, ok := w.(sseResponse); ok {
-			sse.Close()
-		}
+		hj, _ := w.(http.Hijacker)
+		c, bufrw, _ := hj.Hijack()
+
+		bufrw.WriteString("0\r\n\r\n")
+		bufrw.Flush()
+		c.Close()
 	}
 }
 
--- stream/stream.go
+++ stream/stream.go
@@ -1,8 +1,10 @@
 package stream
 
 import (
+	"errors"
 	"io"
 
+	"github.com/benburkert/http"
 	"github.com/garyburd/redigo/redis"
 )
 
@@ -17,7 +19,14 @@ func In(owner, name string, conn redis.Conn) (s Stream, err error) {
 	s = Stream{owner, name, Closed, 0, conn}
 	err = s.open()
 
-	return s, err
+	return
+}
+
+func Out(owner, name string, conn redis.Conn) (s Stream, err error) {
+	s = Stream{owner, name, Closed, 0, conn}
+	s.State, err = s.getState()
+
+	return
 }
 
 type Stream struct {
@@ -29,7 +38,7 @@ type Stream struct {
 	conn redis.Conn
 }
 
-func (s Stream) Fill(r io.Reader) (c int, err error) {
+func (s Stream) From(r io.Reader) (c int, err error) {
 	buf := make([]byte, 4096)
 
 	defer s.close()
@@ -46,6 +55,14 @@ func (s Stream) Fill(r io.Reader) (c int, err error) {
 			s.write(buf[:n])
 			c += n
 		}
+	}
+}
+
+func (s Stream) To(w io.Writer) (int, error) {
+	if s.State == Opened {
+		return s.streamTo(w)
+	} else {
+		return s.getTo(w)
 	}
 }
 
@@ -92,4 +109,77 @@ func (s Stream) streamKey() string {
 
 func (s Stream) nwo() string {
 	return s.Owner + "/" + s.Name
+}
+
+func (s Stream) getState() (State, error) {
+	state, err := redis.Int(s.conn.Do("GET", s.stateKey()))
+
+	return State(state), err
+}
+
+func (s Stream) streamTo(w io.Writer) (int, error) {
+	if buf, err := s.getAndSubscribe(); err != nil {
+		return len(buf), err
+	} else {
+		if n, err := w.Write(buf); err != nil {
+			return n, err
+		} else {
+			w.(http.Flusher).Flush() // hack
+			m, err := s.streamChannel(w)
+
+			return n + m, err
+		}
+	}
+}
+
+func (s Stream) getAndSubscribe() (buf []byte, err error) {
+	s.conn.Send("MULTI")
+	s.conn.Send("GET", s.dataKey())
+	s.conn.Send("SUBSCRIBE", s.streamKey())
+
+	if data, err := redis.Values(s.conn.Do("EXEC")); err != nil {
+		return buf, err
+	} else {
+		redis.Scan(data, &buf)
+
+		return buf, nil
+	}
+}
+
+func (s Stream) getTo(w io.Writer) (int, error) {
+	if data, err := redis.String(s.conn.Do("GET", s.dataKey())); err != nil {
+		return 0, err
+	} else {
+		w.Write([]byte(data))
+		w.(http.Flusher).Flush() // hack
+
+		return len(data), nil
+	}
+}
+
+func (s Stream) streamChannel(w io.Writer) (n int, err error) {
+	psc := redis.PubSubConn{s.conn}
+
+	for {
+		switch v := psc.Receive().(type) {
+		case redis.Message:
+			state, data := State(v.Data[0]), v.Data[1:]
+
+			switch state {
+			case Opened:
+				n += len(data)
+
+				w.Write(data)
+				w.(http.Flusher).Flush() // hack
+			case Closed:
+				return
+			}
+		case error:
+			err = error(v)
+
+			return
+		default:
+			return n, errors.New("Unrecognized redis message")
+		}
+	}
 }
