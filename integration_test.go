a6d5a7d8973ef6f5ac237bee7c4a961c6da9eafb
Ben Burkert <ben@benburkert.com>
2014-06-24 16:53:48 -0700

    Add a lockstep test to assert streaming.

--- integration_test.go
+++ integration_test.go
@@ -3,6 +3,7 @@
 package htee
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http/httptest"
@@ -42,5 +43,45 @@ func TestHelloWorldRoundTrip(t *testing.T) {
 
 	if string(rb) != "Hello, World!" {
 		t.Errorf("Response body is '%s', want 'Hello, World!'", rb)
+	}
+}
+
+func TestStreamingLockstep(t *testing.T) {
+	ts := httptest.NewServer(ServerHandler())
+	defer ts.Close()
+
+	client := testClient(ts.URL)
+	pr, pw := io.Pipe()
+	step, parts := make(chan interface{}), 100
+
+	go func() {
+		if _, err := client.PostStream("lockstep", pr); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	go func() {
+		for i := 1; i <= parts; i++ {
+			pw.Write([]byte(fmt.Sprintf("Part %d", i)))
+			<-step
+		}
+		pw.Close()
+	}()
+
+	res, err := client.GetStream(client.Username, "lockstep")
+	if err != nil {
+		t.Error(err)
+	}
+
+	buf := make([]byte, 4096)
+	for i := 1; i <= parts; i++ {
+		n, err := res.Body.Read(buf)
+		if err != nil {
+			t.Error(err)
+		} else if string(buf[:n]) != fmt.Sprintf("Part %d", i) {
+			t.Errorf("Response part is '%s', want 'Part %d'", buf[:n], i)
+		}
+
+		step <- true
 	}
 }
